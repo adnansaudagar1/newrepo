7b5034480146b59b4eaee493d37da3510f68e23b
Ubuntu <ubuntu@ip-172-31-12-38.ap-south-1.compute.internal>
2025-01-15 10:23:59 +0000

    done1

--- myfile.java
+++ myfile.java
@@ -2,6 +2,7 @@ class myfile
 {
     public static void main(String[] args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("My First Java Program1.");
+
     }
 }
